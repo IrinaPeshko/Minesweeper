14e60fcab23d908c4c362834bd2239928dff5339
IrinaPeshko <privet-kira1999@mail.ru>
2023-05-22 17:38:24 +0300

    refactor:  put function count to another file

---
+++ scripts/countMinesArraund.js
@@ -0,0 +1,46 @@
+export function count(current, boardWidth, minesArr, visited) {
+  let currentId = +current.id;
+  visited.add(currentId);
+
+  let i = 0;
+  const directions = [-1, 0, 1];
+
+  for (let dy of directions) {
+    for (let dx of directions) {
+      if (dx === 0 && dy === 0) continue;
+      const neighborId = currentId + dx + dy * boardWidth;
+
+      // Проверяем, является ли текущая ячейка крайней левой ячейкой
+      if (currentId % boardWidth === 0 && dx === -1) continue;
+
+      // Проверяем, является ли текущая ячейка крайней правой ячейкой
+      if ((currentId + 1) % boardWidth === 0 && dx === 1) continue;
+
+      if (minesArr.includes(neighborId)) i++;
+    }
+  }
+
+  current.innerText = i;
+
+  if (i === 0) {
+    current.innerText = "";
+    for (let dy of directions) {
+      for (let dx of directions) {
+        const neighborId = currentId + dx + dy * boardWidth;
+        if (dx === 0 && dy === 0) continue;
+
+        // Проверяем, является ли текущая ячейка крайней левой ячейкой
+        if (currentId % boardWidth === 0 && dx === -1) continue;
+
+        // Проверяем, является ли текущая ячейка крайней правой ячейкой
+        if ((currentId + 1) % boardWidth === 0 && dx === 1) continue;
+
+        const neighborObject = document.getElementById(`${neighborId}`);
+        if (neighborObject && !visited.has(neighborId)) {
+          count(neighborObject, boardWidth, minesArr, visited);
+          neighborObject.classList.add("gameboard__cell_active");
+        }
+      }
+    }
+  }
+}
--- scripts/onCellClick.js
+++ scripts/onCellClick.js
@@ -1,4 +1,6 @@
 import { createMines } from "./createMines.js";
+import { count } from "./countMinesArraund.js";
+
 export function onCellClick(size) {
   let minesArr;
   let countClicks = 0;
@@ -31,51 +33,6 @@ export function onCellClick(size) {
   })
   }
 
-  function count(current, boardWidth, minesArr, visited) {
-    let currentId = +current.id;
-    visited.add(currentId);
-
-    let i = 0;
-    const directions = [-1, 0, 1];
-
-    for (let dy of directions) {
-      for (let dx of directions) {
-        if (dx === 0 && dy === 0) continue;
-        const neighborId = currentId + dx + dy * boardWidth;
-
-        // Проверяем, является ли текущая ячейка крайней левой ячейкой
-        if (currentId % boardWidth === 0 && dx === -1) continue;
-
-        // Проверяем, является ли текущая ячейка крайней правой ячейкой
-        if ((currentId + 1) % boardWidth === 0 && dx === 1) continue;
-
-        if (minesArr.includes(neighborId)) i++;
-      }
-    }
-
-    current.innerText = i;
-
-    if (i === 0) {
-      current.innerText = "";
-      for (let dy of directions) {
-        for (let dx of directions) {
-          const neighborId = currentId + dx + dy * boardWidth;
-          if (dx === 0 && dy === 0) continue;
-
-          // Проверяем, является ли текущая ячейка крайней левой ячейкой
-          if (currentId % boardWidth === 0 && dx === -1) continue;
-
-          // Проверяем, является ли текущая ячейка крайней правой ячейкой
-          if ((currentId + 1) % boardWidth === 0 && dx === 1) continue;
-
-          const neighborObject = document.getElementById(`${neighborId}`);
-          if (neighborObject && !visited.has(neighborId)) {
-            count(neighborObject, boardWidth, minesArr, visited);
-            neighborObject.classList.add("gameboard__cell_active");
-          }
-        }
-      }
-    }
-  }
+  
 
 
